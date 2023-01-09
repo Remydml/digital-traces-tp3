65a4cff97320bf237e9c543a5ff7f243f325eb67
Rémy Dumoulin <81318770+Remydml@users.noreply.github.com>
2023-01-09 10:59:37 +0100

    Add timer log

---
+++ main.py
@@ -0,0 +1,73 @@
+from flask import Flask
+from pytrends.request import TrendReq
+from io import BytesIO
+import matplotlib.pyplot as plt
+import os
+import base64
+import numpy as np
+from timer_log import timed , count_dict, count_function,run_100times
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+@timed
+def google_trend():
+    pytrends = TrendReq(hl='en-US', tz=360)
+    pytrends.build_payload(["Football World Cup"], timeframe="all")
+    iot = pytrends.interest_over_time() 
+
+    plt.plot(iot['Football World Cup'])
+    plt.xlabel('Date')
+    plt.ylabel('Trend')
+
+    
+    B = BytesIO()
+    plt.savefig(B, format='png')
+    B.seek(0)
+
+    chart = base64.b64encode(B.getvalue()).decode()
+    plt.clf()
+
+    return '<img src="data:image/png;base64,{}">'.format(chart)
+
+@app.route('/experiment', methods=["GET"])
+#@timed
+def experiment():
+  dictionary , function = run_100times()
+
+  dictionnary_mean = str("dictionnary mean : ",np.mean(dictionary))
+  counter_mean= str("Function mean : ",np.mean(function))
+
+  dictionnary_var = str("dictionnary variance : ",np.std(dictionary))
+  counter_var= str("Function variance : ",np.std(function))
+
+  x = np.linspace(0,100, 100)
+
+  plt.plot(x, dictionary)
+  plt.plot(x, function)
+
+  plt.legend(["dictionnary", "fonction counter"], loc ="upper right")
+  plt.xlabel('n times ')
+  plt.ylabel('times')
+
+
+  B = BytesIO()
+  plt.savefig(B, format='png')
+  B.seek(0)
+
+  chart = base64.b64encode(B.getvalue()).decode()
+  plt.clf()
+
+  return dictionnary_mean,counter_mean,dictionnary_var,counter_var,'<img src="data:image/png;base64,{}">'.format(chart)
+
+@app.route('/countDict', methods=["GET"])
+@timed
+def countDict():
+  method_dict = count_dict('t8.shakespeare.txt')
+  return method_dict
+
+@app.route('/countFunc', methods=["GET"])
+@timed
+def countFunc():
+  method_func = count_function('t8.shakespeare.txt')
+  return method_func
